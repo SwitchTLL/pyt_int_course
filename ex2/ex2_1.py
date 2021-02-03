9c0439733eb487adccd802f891d2f5b05bd00674
SwitchTLL <aperturedg@gmail.com>
2021-02-04 01:05:43 +0200

    ver_fin

--- ex2/ex2_1.py
+++ ex2/ex2_1.py
@@ -78,18 +78,4 @@
 #
 # triangle_square = ((half_perimeter * (half_perimeter - side_a) * (half_perimeter - side_b) * (half_perimeter - side_c)) ** 0.5
 # print(triangle_square)
-#
-#
-# side_a = float(input('Please enter side A: '))
-# side_b = float(input('Please enter side B: '))
-# side_c = float(input('Please enter side C: '))
-#
-# half_perimeter = (side_a + side_b + side_c) / 2
-# #print(half_perimeter)
-#
-# triangle_square = (half_perimeter * (half_perimeter - side_a) * (half_perimeter - side_b) * (half_perimeter - side_c)) ** 0.5
-# print(triangle_square)
-#
-# split()  #разделение переменных
-#
-#
+
